43a276d41cdac2d28761948e5965fbc5a9be318b
salam-dalloul <salamdalloul93@gmail.com>
2017-12-12 14:45:01 +0200

    connection function

--- database/build.js
+++ database/build.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
+
 const dbConnection = require('./dbconnection');
+
 const sql = fs.readFileSync(`${__dirname}/build.sql`).toString();
-dbConnection.query(sql, (err,res)=>{
-  if(err) console.log("Query error")
-  else {
-    console.log("response is ", res);
+
+dbConnection.query(sql, (err, res) => {
+  if(err){
+    console.log(err);
+  } else {
+    console.log(`response is ${res}`);
   }
+
 });
